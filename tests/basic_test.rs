1c82ff3b36adf25bed08707a52c6b33c40a03eb3
= <=>
2026-07-05 04:42:30 +0100

    added basic unit test

---
+++ tests/basic_test.rs
@@ -0,0 +1,21 @@
+#[cfg(test)]
+mod basic_test {
+    use assert_cmd::Command;
+    const APP_NAME: &str = "rust-image-by-json";
+
+    fn run_json_instructions(json_file_path: &str) -> Result<(), String> {
+        let mut cmd = Command::cargo_bin(APP_NAME).unwrap();
+
+        cmd.arg("--json-file-path").arg(json_file_path);
+
+        let output = cmd.unwrap();
+
+        Ok(())
+    }
+
+    #[test]
+    fn basic_json() {
+       let _ = run_json_instructions("test-json/basic.json").unwrap();
+    }
+
+}
