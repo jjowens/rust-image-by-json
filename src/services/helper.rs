9002c699547366823f93187919ae607c7cfb0576
= <=>
2026-07-29 18:36:29 +0100

    removed unneeded functions

--- src/services/helper.rs
+++ src/services/helper.rs
@@ -1,45 +1,7 @@
 use std::path::{Path, PathBuf};
 use image::ImageFormat;
-use crate::services::models::save_file_type::SaveFileType;
 use image::imageops::GaussianBlurParameters;
 use crate::services::models::blur_type::BlurType;
-
-pub fn get_save_file_type(file_extension: &String) -> SaveFileType {
-    let save_file_type = match file_extension.as_str() {
-        "tiff" => { SaveFileType::Tiff},
-        "png" =>  { SaveFileType::Png},
-        "gif" => { SaveFileType::Gif},
-        "webp" => { SaveFileType::Webp},
-        "jpeg" => { SaveFileType::Jpeg},
-        "jpg" => { SaveFileType::Jpg},
-        "bmp" => { SaveFileType::Bmp},
-        "tga" => { SaveFileType::Tga},
-        "ico" => { SaveFileType::Ico},
-        "hdr" => { SaveFileType::Hdr},
-        "avif" => { SaveFileType::Avif},
-        _ => { SaveFileType::Jpg}
-    };
-
-    save_file_type
-}
-
-pub fn get_image_format(save_file_type: SaveFileType) -> ImageFormat {
-    let image_file_type = match save_file_type {
-        SaveFileType::Tiff => { ImageFormat::Tiff },
-        SaveFileType::Png => { ImageFormat::Png},
-        SaveFileType::Gif => { ImageFormat::Gif},
-        SaveFileType::Webp => { ImageFormat::WebP},
-        SaveFileType::Jpeg => { ImageFormat::Jpeg},
-        SaveFileType::Jpg => { ImageFormat::Jpeg},
-        SaveFileType::Bmp => { ImageFormat::Bmp},
-        SaveFileType::Tga => { ImageFormat::Tga},
-        SaveFileType::Ico => { ImageFormat::Ico},
-        SaveFileType::Hdr => { ImageFormat::Hdr},
-        SaveFileType::Avif => { ImageFormat::Avif}
-    };
-    
-    image_file_type
-}
 
 pub fn get_file_extension_on_image_format(image_format: ImageFormat) -> String {
     let file_extension = match image_format {
--- src/services/models/mod.rs
+++ src/services/models/mod.rs
@@ -1,5 +1,4 @@
 pub mod process_type;
-pub mod save_file_type;
 pub mod blur_type;
 pub mod instruction;
 pub mod image_config;
